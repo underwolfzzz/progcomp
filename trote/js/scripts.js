9ea2fe3ba79b78e7d6d04cfdf40b8df5a6544c8e
Yago <yago2007henrique@gmail.com>
2025-04-10 20:37:34 -0300

    Novas contas

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -30,16 +30,84 @@ function calcular(){
         }
     }
     else if(equipe=="preto"){
-
+        if(kit >= 103 && suplemento>=52 ){
+            soma = soma + 5000 + ((kit-103)*30) + ((suplemento-52)*15)
+        }
+        else if (kit >= 82 && suplemento>=42){
+            soma = soma + 4000 + ((kit-82)*30) + ((suplemento-42)*15)
+        }
+        else if (kit >= 52 && suplemento>=26){
+            soma = soma + 2500 + ((kit-52)*30) + ((suplemento-26)*15)
+        }
+        else if (kit >= 21 && suplemento>=10){
+            soma = soma + 1000 + ((kit-21)*30) + ((suplemento-10)*15)
+        }
+        if (sangue >= 52){
+            soma = soma + 2500 + ((sangue - 52) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
+        }
     }
     else if(equipe=="roxo"){
-
+        if(kit >= 102 && suplemento>=51 ){
+            soma = soma + 5000 + ((kit-102)*30) + ((suplemento-52)*15)
+        }
+        else if (kit >= 82 && suplemento>=41){
+            soma = soma + 4000 + ((kit-82)*30) + ((suplemento-41)*15)
+        }
+        else if (kit >= 51 && suplemento>=26){
+            soma = soma + 2500 + ((kit-51)*30) + ((suplemento-26)*15)
+        }
+        else if (kit >= 20 && suplemento>=10){
+            soma = soma + 1000 + ((kit-20)*30) + ((suplemento-10)*15)
+        }
+        if (sangue >= 51){
+            soma = soma + 2500 + ((sangue - 51) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
+        }
     }
     else if(equipe=="verde"){
-
+        if(kit >= 88 && suplemento>=44 ){
+            soma = soma + 5000 + ((kit-88)*30) + ((suplemento-44)*15)
+        }
+        else if (kit >= 70 && suplemento>=35){
+            soma = soma + 4000 + ((kit-70)*30) + ((suplemento-35)*15)
+        }
+        else if (kit >= 44 && suplemento>=22){
+            soma = soma + 2500 + ((kit-44)*30) + ((suplemento-22)*15)
+        }
+        else if (kit >= 18 && suplemento>=9){
+            soma = soma + 1000 + ((kit-18)*30) + ((suplemento-9)*15)
+        }
+        if (sangue >= 44){
+            soma = soma + 2500 + ((sangue - 44) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
+        }
     }
     else{
-
+        if(kit >= 93 && suplemento>=47 ){
+            soma = soma + 5000 + ((kit-93)*30) + ((suplemento-47)*15)
+        }
+        else if (kit >= 74 && suplemento>=38){
+            soma = soma + 4000 + ((kit-74)*30) + ((suplemento-38)*15)
+        }
+        else if (kit >= 47 && suplemento>=24){
+            soma = soma + 2500 + ((kit-47)*30) + ((suplemento-24)*15)
+        }
+        else if (kit >= 19 && suplemento>=9){
+            soma = soma + 1000 + ((kit-19)*30) + ((suplemento-9)*15)
+        }
+        if (sangue >= 47){
+            soma = soma + 2500 + ((sangue - 47) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
+        }
     }
 
 
